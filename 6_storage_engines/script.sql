420672094d53970ffbbbba242617cb521e36d281
MUGISHA <166393807+Manege24@users.noreply.github.com>
2024-11-21 00:50:13 +0300

    Engines

--- 6_storage_engines/script.sql
+++ 6_storage_engines/script.sql
@@ -1,1 +1,27 @@
 -- Write your SQL code here
+-- For Customer Entity 
+ENGINE = InnoDB
+ENGINE = My MyISAM
+
+-- For Employee Entity
+ENGINE = Memory
+ENGINE = InnoDB
+
+-- For Shareholder Entity 
+ENGINE = InnoDB
+ENGINE = MyISAM
+
+-- For Calls Entity
+ENGINE = InnoDB
+
+-- MPesa Entity
+ENGINE = InnoDB
+
+-- Support Entity
+ENGINE = InnoDB
+-- SimCard Entity
+ENGINE = InnoDB
+-- Income Entity
+ENGINE = MyISAM
+-- Payment Entity
+ENGINE = InnoDB
