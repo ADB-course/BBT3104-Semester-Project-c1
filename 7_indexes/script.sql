1357c6865c69cc63b58d772b8bb757ccb579806f
MUGISHA <166393807+Manege24@users.noreply.github.com>
2024-11-21 00:52:13 +0300

    Indexes

--- 7_indexes/script.sql
+++ 7_indexes/script.sql
@@ -1,1 +1,28 @@
 -- Write your SQL code here
+-- For Customer Entity 
+Index = B+Tree
+
+-- For Employee Entity
+Index = B+Tree
+
+-- For Shareholder Entity 
+Index = B+Tree
+
+-- For Calls Entity
+Index = B+Tree
+index = Hash
+
+-- MPesa Entity
+Index = B+Tree
+index = Hash
+
+-- Support Entity
+Index = B+Tree
+index = Hash
+-- SimCard Entity
+Index = B+Tree
+-- Income Entity
+Index = B+Tree
+-- Payment Entity
+Index = B+Tree
+index = Hash
