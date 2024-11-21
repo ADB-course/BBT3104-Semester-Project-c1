d173c543ad2d120aa9a9cbc5e4b157400f8ffcda
Omardecoder <146336046+Omardecoder@users.noreply.github.com>
2024-11-21 15:51:03 +0300

    Normal Triggers

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,1 +1,10 @@
 -- Write your SQL code here
+CREATE TRIGGER before_sim_card_deletion
+BEFORE DELETE OF Sim card No.
+ON Sim card
+BEGIN
+    IF OLD.status = 'active' THEN
+        SIGNAL SQL STATE '45000'
+        SET MESSAGE_TEXT = 'Cannot delete a sim card with an active status.';
+    END IF;
+END;
